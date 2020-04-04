23cfd17beef458817e80d5c3970c35cedae7986f
Kawin Nikomborirak <concavemail@gmail.com>
2020-04-04 01:49:53 +0000

    Remove some for loop levels with function calls

--- kfda.py
+++ kfda.py
@@ -20,33 +20,32 @@ class Kfda(BaseEstimator, ClassifierMixin):
         m_star = np.zeros(n_datapoints)
 
         for i in range(n_datapoints):
-            for datapoint in X:
-                m_star[i] += self.kernel(X[i], datapoint) / n_datapoints
+            m_star[i] = self.kernel(X[i], X).mean()
 
         m_classes = {}
         H_classes = {}
         K_classes = {}
 
-        for label in y:
+        classes_datapoints = {}
+        n_classes_datapoints = {}
+
+        for label in self.classes_:
             m_class = m_classes[label] = np.zeros(n_datapoints)
-            class_datapoints = X[y == label]
-            n_class_datapoints = class_datapoints.shape[0]
+            class_datapoints = classes_datapoints[label] = X[y == label]
+            n_class_datapoints = n_classes_datapoints[label] = class_datapoints.shape[0]
             H_classes[label] = np.eye(
                 n_class_datapoints) - 1 / n_class_datapoints
             K_class = K_classes[label] = np.zeros(
                 (n_datapoints, n_class_datapoints))
 
             for i, datapoint in enumerate(X):
-                for j, (class_datapoint) in enumerate(class_datapoints):
-                    m_class[i] += self.kernel(
-                        X[i], class_datapoint) / n_class_datapoints
-
-                    K_class[i, j] = self.kernel(datapoint, class_datapoint)
+                K_class[i] = self.kernel(datapoint, class_datapoints)
+                m_class[i] = K_class[i].mean()
 
         M = np.zeros((n_datapoints, n_datapoints))
         N = np.zeros((n_datapoints, n_datapoints))
 
-        for label in y:
+        for label in self.classes_:
             m_class = m_classes[label]
             m_class_centered = m_class - m_star
             M += np.outer(m_class_centered, m_class_centered)
@@ -56,11 +55,22 @@ class Kfda(BaseEstimator, ClassifierMixin):
             N += K_class.dot(H_class).dot(K_class.T)
 
         w, v = eig(M, N)
-        v = v[np.argsort(w)]
+        v = v[:, np.argsort(w)[:-self.n_components-1:-1]]
 
         self.weights_ = v
 
+        self.centers_ = np.empty((self.classes_.size, self.n_components))
+
+        for i, label in enumerate(self.classes_):
+            for class_datapoint in classes_datapoints[label]:
+                self.centers_[
+                    i] += self.weights_.T.dot(self.kernel(X, class_datapoint))
+            self.centers_[i] /= n_classes_datapoints[label]
+
         return self
+
+    def project_point_1d(self, x):
+        return self.weights_ * self.kernel(self.X_, x)
 
     def predict(self, X):
         check_is_fitted(self)
