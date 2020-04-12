080dcb72c281c202d8f63084cfc3324a968c0f81
Kawin Nikomborirak <concavemail@gmail.com>
2020-04-12 19:36:38 +0000

    Use lambda for rbf

--- kfda.py
+++ kfda.py
@@ -5,6 +5,10 @@ from scipy.linalg import eig
 import numpy as np
 
 
+def centering_matrix(n):
+    return np.eye(n) - 1 / n
+
+
 class Kfda(BaseEstimator, ClassifierMixin):
     def __init__(self, n_components=2, kernel=lambda a, b: np.inner(a, b)):
         self.kernel = kernel
@@ -18,51 +22,44 @@ class Kfda(BaseEstimator, ClassifierMixin):
 
         n_datapoints = y.size
 
-        m_star = np.array([self.kernel(datapoint, X).mean()
-                          for datapoint in X])
-
-        m_classes = {}
-        H_classes = {}
-        K_classes = {}
-
         classes_datapoints = {}
-        n_classes_datapoints = {}
 
-        for label in self.classes_:
-            class_datapoints = classes_datapoints[label] = X[y == label]
-            n_class_datapoints = n_classes_datapoints[label] = class_datapoints.shape[0]
-            H_classes[label] = np.eye(
-                n_class_datapoints) - 1 / n_class_datapoints
-
-            K_classes[label] = np.array(
-                [self.kernel(datapoint, class_datapoints) for datapoint in X])
-            m_classes[label] = K_classes[label].mean(-1)
-
+        # Find M and N
         M = np.zeros((n_datapoints, n_datapoints))
         N = np.zeros((n_datapoints, n_datapoints))
 
-        M = np.sum([np.outer(m_class - m_star, m_class - m_star)
-                   for m_class in m_classes.values()], 0)
-        N = np.sum([K_classes[label].dot(H_classes[label]).dot(
-            K_classes[label].T) for label in self.classes_], 0)
+        m_star = np.array([self.kernel(datapoint, X).mean()
+                          for datapoint in X])
 
+        for label in self.classes_:
+            class_datapoints = classes_datapoints[label] = X[y == label]
+            n_class_datapoints = class_datapoints.shape[0]
 
+            H = centering_matrix(n_class_datapoints)
+            K = np.array([self.kernel(datapoint, class_datapoints)
+                         for datapoint in X])
+            m = K.mean(-1)
+
+            m_centered = m - m_star
+            M += np.outer(m_centered, m_centered)
+            N += K.dot(H).dot(K.T)
+
+        # Find weights
         w, v = eig(M, N)
 
-        w_mask = ~np.isinf(w) & ~np.isnan(w)
-        w = w[w_mask]
-        v = v[:, w_mask]
+        w_finite_mask = ~(np.isinf(w) | np.isnan(w))
+        w = w[w_finite_mask]
+        v = v[:, w_finite_mask]
         v = v[:, np.argsort(w)[:-self.n_components-1:-1]]
 
         self.weights_ = v.astype(np.float64)
 
-        self.centers_ = np.empty((self.classes_.size, self.n_components))
-
-        for i, label in enumerate(self.classes_):
-            for class_datapoint in classes_datapoints[label]:
-                self.centers_[
-                    i] += self.weights_.T.dot(self.kernel(X, class_datapoint))
-            self.centers_[i] /= n_classes_datapoints[label]
+        # Compute centers
+        self.centers_ = np.array([
+            np.mean([
+                self.weights_.T.dot(self.kernel(X, class_datapoint))
+                for class_datapoint in classes_datapoints[label]], 0)
+            for label in self.classes_])
 
         return self
 
--- test_kfda.py
+++ test_kfda.py
@@ -5,27 +5,20 @@ import matplotlib.pyplot as plt
 
 from kfda import Kfda
 
-cls = Kfda()
-# X, y = fetch_openml('mnist_784', version=1, return_X_y=True)
 
-# new_dataset = []
-# new_labels = []
-# for label in unique_labels(y):
-#     sample = list(X[y==label][:100])
-#     new_dataset.extend(sample)
-#     new_labels.extend([label] * 100)
+def rbf(variance):
+    return lambda a, b: np.exp((((a - b) / np.sqrt(variance))**2).sum(-1) / 2)
 
 
-# new_dataset = np.array(new_dataset)
-# new_labels = np.array(new_labels)
-# np.save('data', new_dataset)
-# np.save('labels', new_labels)
+cls = Kfda(kernel=rbf(1e8))
 X = np.load('data.npy')
 y = np.load('labels.npy')
 
 print("Fitting")
 cls.fit(X, y)
+
 points = cls.project_points(X).reshape(10, 10, -1)
+
 for pointset in points:
     plt.plot(pointset[:, 0], pointset[:, 1], '.')
 plt.show()
